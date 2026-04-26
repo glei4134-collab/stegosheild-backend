05741c3a8fa8f3d820387afeb5830565b86e6167
lbh <glei4134@gmail.com>
2026-04-26 13:43:03 +0800

    Add WebSocket support for real-time notifications

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os
-from app import create_app
-
-app = create_app()
+from app import create_app, socketio
 
 if __name__ == '__main__':
     host = os.getenv('FLASK_HOST', '0.0.0.0')
     port = int(os.getenv('PORT', 5001))
     debug = False
 
-    app.run(host=host, port=port, debug=debug)
+    socketio.run(app, host=host, port=port, debug=debug)
